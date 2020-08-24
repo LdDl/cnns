723e179a40e2ad4752d8107d4a701cf301558072
Dimitrii <sexykdi@gmail.com>
2020-08-24 09:05:47 +0300

    remove excess pointer symbol

--- gradient.go
+++ gradient.go
@@ -49,34 +49,34 @@ func NewTensorGradient(x, y, z int) TensorGradient {
 
 // Get - Return (i,j,k)-th gradient from tensor.
 func (t1 *TensorGradient) Get(x, y, z int) Gradient {
-	return (*t1).Data[z*(*t1).Size.X*(*t1).Size.Y+y*(*t1).Size.X+x]
+	return t1.Data[z*t1.Size.X*t1.Size.Y+y*t1.Size.X+x]
 }
 
 // Set - Set (i,j,k)-th gradient with new gradient value
 func (t1 *TensorGradient) Set(x, y, z int, val Gradient) {
-	(*t1).Data[z*(*t1).Size.X*(*t1).Size.Y+y*(*t1).Size.X+x] = val
+	t1.Data[z*t1.Size.X*t1.Size.Y+y*t1.Size.X+x] = val
 }
 
 // SetGrad - Set (i,j,k)-th gradient's current value with new one.
 func (t1 *TensorGradient) SetGrad(x, y, z int, val float64) {
-	(*t1).Data[z*(*t1).Size.X*(*t1).Size.Y+y*(*t1).Size.X+x].Grad = val
+	t1.Data[z*t1.Size.X*t1.Size.Y+y*t1.Size.X+x].Grad = val
 }
 
 // AddToGrad - Add float64 to (i,j,k)-th gradient's current value.
 func (t1 *TensorGradient) AddToGrad(x, y, z int, val float64) {
-	(*t1).Data[z*(*t1).Size.X*(*t1).Size.Y+y*(*t1).Size.X+x].Grad += val
+	t1.Data[z*t1.Size.X*t1.Size.Y+y*t1.Size.X+x].Grad += val
 }
 
 // Print - Pretty print tensor of gradients.
 func (t1 *TensorGradient) Print() {
-	mx := (*t1).Size.X
-	my := (*t1).Size.Y
-	mz := (*t1).Size.Z
+	mx := t1.Size.X
+	my := t1.Size.Y
+	mz := t1.Size.Z
 	for z := 0; z < mz; z++ {
 		fmt.Printf("Dim: %v\n", z)
 		for y := 0; y < my; y++ {
 			for x := 0; x < mx; x++ {
-				fmt.Printf("%.8f\t", (*t1).Get(x, y, z))
+				fmt.Printf("%.8f\t", t1.Get(x, y, z))
 			}
 			fmt.Println()
 		}
--- tensor/convolve.go
+++ tensor/convolve.go
@@ -2,8 +2,8 @@ package tensor
 
 // Conv2D - @experimental Convolve tensor with respect only to X and Y axis (2D)
 func (t1 *Tensor) Conv2D(t2 Tensor, stride [2]int, padding [2]int) Tensor {
-	outputX := ((*t1).Size.X-t2.Size.X+2*padding[0])/stride[0] + 1
-	outputY := ((*t1).Size.Y-t2.Size.Y+2*padding[1])/stride[1] + 1
+	outputX := (t1.Size.X-t2.Size.X+2*padding[0])/stride[0] + 1
+	outputY := (t1.Size.Y-t2.Size.Y+2*padding[1])/stride[1] + 1
 	outputData := NewTensor(outputX, outputY, t2.Size.Z)
 	el := 0
 	for i := 0; i < outputY; i = (i + stride[0]) {
@@ -17,14 +17,14 @@ func (t1 *Tensor) Conv2D(t2 Tensor, stride [2]int, padding [2]int) Tensor {
 				for n := 0; n < t2.Size.Y; n++ {
 					ii := i + m
 					jj := j + n
-					if ii >= 0 && ii < (*t1).Size.Y && jj >= 0 && jj < (*t1).Size.X {
+					if ii >= 0 && ii < t1.Size.Y && jj >= 0 && jj < t1.Size.X {
 						outputElement := 0.0
 						kernelElement := t2.Data[(m*t2.Size.X + n)]
 						if i == 0 {
-							inputElement := (*t1).Data[((m)*t1.Size.X+n)+j]
+							inputElement := t1.Data[((m)*t1.Size.X+n)+j]
 							outputElement = inputElement * kernelElement
 						} else {
-							inputElement := (*t1).Data[ii*(*t1).Size.X+jj]
+							inputElement := t1.Data[ii*t1.Size.X+jj]
 							outputElement = inputElement * kernelElement
 						}
 						outputData.Data[el] += outputElement
